5e10723dd6d35a24b910b7dc60d5593fcb7c96b8
nagubandigopal <nagubandigopal@gmail.com>
2019-02-24 21:14:57 +0530

    procfile coomit

--- drf_with_heroku/drf_with_heroku/settings.py
+++ drf_with_heroku/drf_with_heroku/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,6 +147,3 @@ DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
