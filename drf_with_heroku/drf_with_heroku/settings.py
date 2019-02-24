a240000df2c756af8a37549e4184b585d91d9d50
nagubandigopal <nagubandigopal@gmail.com>
2019-02-24 19:52:50 +0530

    my commit for upd

--- drf_with_heroku/drf_with_heroku/settings.py
+++ drf_with_heroku/drf_with_heroku/settings.py
@@ -140,10 +140,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
